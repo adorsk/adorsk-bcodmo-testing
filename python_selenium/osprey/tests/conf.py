ec0bbf496e1fd34dafabe937c5dc2734d0319f88
adorsk@whoi.edu <adorsk@whoi.edu>
2012-03-24 21:18:50 -0400

    changed test dir to be configured from env variables

--- python_selenium/osprey/tests/conf.py
+++ python_selenium/osprey/tests/conf.py
@@ -5,20 +5,21 @@ import os
 
 conf = {
         "base_url": '',
-        "output_dir": 'test-output',
         "timeout": 10,
-        "selenium_jar": ''
+        "selenium_jar": '',
+        "test_report_dir": ''
         }
-
-xml_runner = xmlrunner.XMLTestRunner(output=conf.get('output_dir', '.'))
-#conf['default_runner'] = xml_runner
-conf['default_runner'] = unittest.TextTestRunner(verbosity=2)
 
 conf.update(secrets.secrets)
 
 # Override from environment variables.
 env_vars = {
-        "SELENIUM_JAR": "selenium_jar"
+        "SELENIUM_JAR": "selenium_jar",
+        "TEST_REPORTS": "test_report_dir"
         }
 for env_var, conf_var in env_vars.items():
     if os.getenv(env_var): conf[conf_var] = os.getenv(env_var)
+
+xml_runner = xmlrunner.XMLTestRunner(output=conf.get('test_report_dir', '.'))
+conf['default_runner'] = xml_runner
+#conf['default_runner'] = unittest.TextTestRunner(verbosity=2)
